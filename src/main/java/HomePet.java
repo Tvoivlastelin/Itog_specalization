456eaf42d624bcfe767cf8ac7ac8c77498735c1d
Roman <vetdzen@gmail.com>
2023-12-08 12:53:19 +0300

    added class home and pack animal

---
+++ src/main/java/HomePet.java
@@ -0,0 +1,5 @@
+class HomePet extends Animal {
+    public HomePet(String name, String birthDate) {
+        super(name, birthDate);
+    }
+}
---
+++ src/main/java/PackAnimal.java
@@ -0,0 +1,5 @@
+class PackAnimal extends Animal {
+    public PackAnimal(String name, String birthDate) {
+        super(name, birthDate);
+    }
+}
